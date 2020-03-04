dafa197cc2a9fbebefe8e5fd1e6830a37b86ec4b
Ryo Yamashita <qryxip@gmail.com>
2020-03-04 20:56:55 +0900

    Implement the commands

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,9 @@
-fn main() {}
+use cargo_scripts::{Context, Opt};
+
+use human_panic::setup_panic;
+use structopt::StructOpt as _;
+
+fn main() -> anyhow::Result<()> {
+    setup_panic!();
+    cargo_scripts::run(Opt::from_args(), Context::new()?)
+}
